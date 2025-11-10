eef4afb2a5dcafd252952632fbb70381a0b44ca0
AJEY2006 <ajayyadav182764@gmail.com>
2025-11-10 19:59:42 +0000

    okay

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,59 @@
+let myNumber = 100;
+//console.log(myNumber);
+
+let myBalance = new Number(4000);
+//console.log(myBalance);
+//console.log(typeof myBalance);
+//console.log(myBalance.toString().length);
+//console.log(typeof myBalance);
+//console.log(myBalance.toFixed(2));
+
+const otherNumber = 768.9556;
+//console.log(otherNumber.toPrecision(3));
+//console.log(otherNumber.toPrecision(4));
+//console.log(otherNumber.toPrecision(2));
+const hundreds = 1000000;
+//console.log(hundreds.toLocaleString());
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+//+++++++++++++++++++ MATH ++++++++++++++++++++++
+/*
+console.log(Math);
+console.log(Math.round(8.8));
+console.log(Math.round(9.1));
+console.log(Math.ceil(0.99));
+console.log(Math.floor(0.99));
+console.log(Math.abs(-2));
+console.log(Math.min(17,34,51,68));
+console.log(Math.max(19,38,17,34,57,51));
+*/
+//console.log(Math.random());
+//console.log(Math.random() * 10);
+//console.log(Math.random() *10  + 8);
+//console.log(Math.floor(Math.random() *10  + 8));
+
+const min = 21;
+const max = 31;
+
+console.log(Math.floor(Math.random() * (max-min+1) +min));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
