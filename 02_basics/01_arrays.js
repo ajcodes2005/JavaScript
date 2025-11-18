c5c279f1796cbe0650c28b073c009ed57dd7cfb1
AJEY2006 <ajayyadav182764@gmail.com>
2025-11-18 19:05:52 +0000

    okay

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,51 @@
+const Arra1 = [7, 111, "Dumbell", "Mountain"] ;
+//console.log(Arra1[2]);
+//console.log(Arra1[0]);
+
+const Scientists = ["Isaac Newton", "Aryabhatta", "Albert Einstein", "Srinivas Ramanujan"]
+//console.log(Scientists);
+//console.log(Scientists.indexOf("Isaac Newton"));
+
+let Arra2 = new Array(false, 19, "Sahil", "Arush")
+//console.log(Arra2);
+Arra2.push("Ajay");
+//console.log(Arra2);
+Arra2.push(19.2);
+//console.log(Arra2);
+Arra2.pop()
+//console.log(Arra2);
+
+let Magic = ["Electricity", "Internet"]
+Magic.unshift("Electrons")
+//console.log(Magic);
+
+const atom = ["Electron", "Proton", "Neutron"];
+atom.shift(0);
+//console.log(atom);
+
+let sky = ["infinite", "Blue", "Eagles"]
+//console.log(sky.includes("clouds"));
+//console.log(sky.includes("Eagles"));
+
+const myArra1 = new Array(13, 15, 17, 19);
+const newArra1 = myArra1.join();
+//console.log(typeof newArra1);
+
+let myArraB = new Array(myArra1)
+console.log( "Z", myArraB);
+const myArraC = myArraB.slice(13,17);
+console.log(myArraC);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
