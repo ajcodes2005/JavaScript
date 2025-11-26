1877751808e16d4e7f4c84ec795f148a61f4ac4e
AJEY2006 <ajayyadav182764@gmail.com>
2025-11-26 03:31:50 +0000

    okay

---
+++ 02_basics/00_practise.js
@@ -0,0 +1,38 @@
+//myLiora[13, 14, 15] = y
+//console.log(y);
+
+/* let ArrayA[X] = W
+const W = [11, 111, 1]
+console.log(W);
+*/
+/*
+const electronics = {
+    one : "Laptop",
+    two : "Headphone",
+    three : "Keyboard",
+    four : "Mouse",
+    five : "Chargers",
+
+    Person : {
+        Model : "Deepseek V-3.2",
+        Unsupervised : true,
+        helpful : "true"
+    },
+    atmosphere: function () {
+        console.log("POLYGON");
+    }
+}
+console.log(electronics);
+*/
+
+let breakfast = ["Milk", "Egg", "Bread", "Banana"]
+console.log(breakfast);
+
+const pattern = [1, 2, 5, 9, 14, 20, 27, 35];
+console.log(pattern);
+
+let 
+
+
+
+
