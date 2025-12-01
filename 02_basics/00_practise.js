8433bac7e0ae28b5af9ed4b4872308f7e50049b4
AJEY2006 <ajayyadav182764@gmail.com>
2025-12-01 15:21:13 +0000

    okay

--- 02_basics/00_practise.js
+++ 02_basics/00_practise.js
@@ -26,10 +26,88 @@ console.log(electronics);
 */
 
 let breakfast = ["Milk", "Egg", "Bread", "Banana"]
-console.log(breakfast);
+//console.log(breakfast);
 
 const pattern = [1, 2, 5, 9, 14, 20, 27, 35];
-console.log(pattern);
+//console.log(pattern);
+
+const Array1 = ["Sun", "Earth", "Moon" ]
+//console.log(Array1);
+//console.log(Array1.length);
+//console.log(Array1.indexOf("Mars"));
+//console.log(Array1.indexOf("Sun"));
+
+const planetA = {
+    name: "Earth",
+    age : 4540000000 ,
+    atmoshphere : true ,
+    dinosuar : false ,
+}
+//console.log(planetA);
+//console.log(planetA.age);
+/*
+const number = {
+   sample : let type = [
+    "Irrational", "Rational", "Integer", "Whole", "Natural"
+   ]
+}
+console.log(number);
+*/
+
+let number = {
+    typeA : "Irrational",
+    Example : "√2",
+    completelyDivisible : false ,
+}
+//console.log(Number);
+//console.log(number.typeA);
+//console.log(number["typeA"]);
+
+let river = {
+    name : "Ganga",
+    distance : "2525km",
+    origin : "Gangotri glacier",
+    sacred : true ,
+}
+//console.log(river);
+//console.log(river.origin);
+
+let fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 787, 1397, 2184]
+//console.log(fibonacci);
+//console.log(fibonacci.length);
+
+let magic = ["electricity" , ["electron"]]
+//console.log(magic);
+//console.log(magic.length);
+//console.log(magic[2]);
+//console.log(magic[1]);
+
+let digit = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, [88253, 56711, "0344AH"]]
+console.log(digit);
+console.log(digit[1] [0]);
+
+const flow = ["elctricity", ["Computer", "Bulb", "Motor"] ]
+console.log(flow);
+console.log(flow[1] [1]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
